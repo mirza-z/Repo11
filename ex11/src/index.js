c782338ec5c846e27485cb1bf9e6fd575d2b8ae8
mirza-z <zaimovic330@gmail.com>
2022-04-08 04:31:12 +0200

    Hello its me again

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -1,10 +1,11 @@
 // Only change code below this line
 function mySplice(arr1, arr2, n){
+    var arr = arr2;
     for(let i=arr1.length-1; i>=0; i--){
-        arr2.splice(n,0,arr1[i]);
+        arr.splice(n,0,arr1[i]);
         n++;
     }
-    return arr2;
+    return arr;
 }
 // Only change code above this line
 console.log(mySplice(["Dell", "Philips"], ["Samsung", "AOX", "Sony"], 2)); //Change this line
