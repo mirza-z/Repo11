f9a341d38461af68dd41f4735ce01d15fbd1c7a9
mirza-z <zaimovic330@gmail.com>
2022-04-08 21:11:36 +0200

    kkk

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,4 +1,3 @@
-// Create a temps array here
 var temps = [
     [31, 32, 19, 37],
     [29, 27, 55, 36],
@@ -6,19 +5,21 @@ var temps = [
     [29, 52, 21, 64],
     [91, 27, 31, 61]
 ];
-// End of temps array
-// Create a function below this line
-function myArrayFunction(){
-    var averageDayTemp=[], sum=0;
-    for(let i=0; i<temps.length; i++){
-        sum=0;
-        for(let j=0; j<temps[i].length; j++){
-            sum += temps[i][j];
+
+function myArrayFunction(temp) {
+    var averageDayTemp = [];
+    var sum = 0;
+
+    for (var i = 0; i < temp.length; i++) {
+        sum = 0;
+        for (var j = 0; j < temp[i].length; j++) {
+            sum += temp[i][j];
         }
-        averageDayTemp.push(sum/temps[i].length);
+        averageDayTemp.push(sum / 4);
     }
     return averageDayTemp;
 }
-console.log(myArrayFunction());
-// End of creating a function
+
+console.log(myArrayFunction(temps));
+
 module.exports = myArrayFunction;
